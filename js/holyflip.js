9d8e4d9462f2513942bb9472d1694d6e1d7366f1
Local Laptop Malchus <mgibbs@marcusgibbs.com>
2014-07-15 11:35:26 -0400

    Touch Ups

--- js/holyflip.js
+++ js/holyflip.js
@@ -2,103 +2,22 @@
 $(document).ready(function(){
 	// Open reload and fade in functions
 	function reloadPage(){location.reload();}
-	$('body').hide().fadeIn(2000) ;	
-
-	// Array for Bible Books
-	var item_array = [
-		"genesis", 
-		"exodus", 
-		"leviticus", 
-		"numbers", 
-		"deuteronomy",
-		"joshua",
-		"judges",
-		"ruth",
-		"1sam",
-		"2sam",
-		"1kings",
-		"2kings",
-		"1chronicles",
-		"2chronicles",
-		"ezra",
-		"nehemiah",
-		"esther",
-		"job",
-		"psalms",
-		"proverbs",
-		"ecclesiastes",
-		"songofsolomon",
-		"isaiah",
-		"jeremiah",
-		"lamentations",
-		"ezekiel",
-		"daniel",
-		"hosea",
-		"joel",
-		"amos",
-		"obadiah",
-		"jonah",
-		"micah",
-		"nahum",
-		"habakkuk",
-		"zephaniah",
-		"haggai",
-		"zechariah",
-		"malachi",
-		"matthew",
-		"mark",
-		"luke",
-		"john",
-		"acts",
-		"romans",
-		"1corinthians",
-		"2corinthians",
-		"galatians",
-		"ephesians",
-		"philippians",
-		"colossians",
-		"1thessalonians",
-		"2thessalonians",
-		"1timothy",
-		"2timothy",
-		"titus",
-		"philemon",
-		"hebrews",
-		"james",
-		"1peter",
-		"2peter",
-		"1john",
-		"2john",
-		"3john",
-		"jude",
-		"revelation"];
 	
+	// Array for Bible Books
+	var item_array = ["genesis", "exodus", "leviticus", "numbers", "deuteronomy","joshua", "judges", "ruth", "1sam", "2sam", "1kings", "2kings", "1chronicles", "2chronicles", "ezra","nehemiah", "esther", "job", "psalms", "proverbs", "ecclesiastes","songofsolomon", "isaiah", "jeremiah", "lamentations","ezekiel","daniel","hosea","joel","amos","obadiah","jonah","micah","nahum","habakkuk","zephaniah","haggai","zechariah","malachi","matthew","mark","luke","john","acts","romans","1corinthians","2corinthians","galatians","ephesians","philippians","colossians","1thessalonians","2thessalonians","1timothy","2timothy","titus","philemon","hebrews","james","1peter","2peter","1john","2john","3john","jude","revelation"];
 	var random_item = item_array[Math.floor(Math.random() * item_array.length)];
+
 	// Write the Verse to the Page
 	$.getJSON('books/'+ random_item + '.json', function(jd) {
+
 		// Finds the number of chapters from a chosen book.
-		function countChapters(obj) {
- 			var prop;
-  			var propCount = 0;
-			for (prop in obj) {
-    				propCount++;
-  			}
-  			return propCount;
-		}
-		
+		function countChapters(obj) {var prop;var propCount = 0;for (prop in obj) {propCount++;}return propCount;}
 		
 		// Randomize the chapter
 		var c = Math.floor((Math.random() * countChapters(jd.book)) + 1);
 		
 		// Find the number of verses in the random chapter
-		function countVerses(obj) {
-                        var prop;
-                        var propCount = 0;
-                        for (prop in obj) {
-                                propCount++;
-                        }
-                        return propCount;
-                }
+		function countVerses(obj) {var prop;var propCount = 0;for (prop in obj) {propCount++;}return propCount;}
 		
 		// Randomize verse number
 		var v = Math.floor((Math.random() * countVerses(jd.book[c].chapter)) + 1);
@@ -128,11 +47,6 @@ $(document).ready(function(){
 		$("body").fadeOut(1000,reloadPage)	
 	});
 
-	$('#backFlip').click(function() {
-		$('#top').slideDown(1000);
-		$('#context').fadeOut(1000);        
-
-	});
 
 
 
