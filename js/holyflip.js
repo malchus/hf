628263386af2cd7ab1573ecf25267876f4c0bf44
malchus <malchus@p3nlhftpg079.shr.prod.phx3.secureserver.net>
2014-07-15 13:23:26 -0700

    Completed building verse transition instead of reloading entire page

--- js/holyflip.js
+++ js/holyflip.js
@@ -3,10 +3,13 @@ $(document).ready(function(){
 	// Open reload and fade in functions
 	function reloadPage(){location.reload();}
 	
+	
+
+$('#holyFlip').click(function() {
 	// Array for Bible Books
 	var item_array = ["genesis", "exodus", "leviticus", "numbers", "deuteronomy","joshua", "judges", "ruth", "1sam", "2sam", "1kings", "2kings", "1chronicles", "2chronicles", "ezra","nehemiah", "esther", "job", "psalms", "proverbs", "ecclesiastes","songofsolomon", "isaiah", "jeremiah", "lamentations","ezekiel","daniel","hosea","joel","amos","obadiah","jonah","micah","nahum","habakkuk","zephaniah","haggai","zechariah","malachi","matthew","mark","luke","john","acts","romans","1corinthians","2corinthians","galatians","ephesians","philippians","colossians","1thessalonians","2thessalonians","1timothy","2timothy","titus","philemon","hebrews","james","1peter","2peter","1john","2john","3john","jude","revelation"];
 	var random_item = item_array[Math.floor(Math.random() * item_array.length)];
-
+	
 	// Write the Verse to the Page
 	$.getJSON('books/'+ random_item + '.json', function(jd) {
 
@@ -22,30 +25,38 @@ $(document).ready(function(){
 		// Randomize verse number
 		var v = Math.floor((Math.random() * countVerses(jd.book[c].chapter)) + 1);
 	
+		
+		
+		
 		// Writes the Verse to the stage
-		$('#stage').html('<h1>' + jd.book_name + " " + jd.book[c].chapter_nr  + ":" + jd.book[c].chapter[v].verse_nr  + '</h1>'); 
-		$('#stage').append('<p> ' + jd.book[c].chapter[v].verse  + '</p>'); 
+	$("#stage").fadeToggle(1000, function(){	
+		$('#stage').html('<h1>' + jd.book_name + " " + jd.book[c].chapter_nr  + ":" + jd.book[c].chapter[v].verse_nr  + '</h1>').append('<p> ' + jd.book[c].chapter[v].verse  + '</p>').fadeToggle(); 
+	
+    });
+        
+        
+        
         
 		// Display the Chapter in Context.	
-		$('#context').fadeIn().append("<h2>" + jd.book_name + " " + jd.book[c].chapter_nr + "</hr>");
+		$('#context').html("<h2>" + jd.book_name + " " + jd.book[c].chapter_nr + "</hr>");
 		var vCount = countVerses(jd.book[c].chapter);
 		var x = 0;
 		while (x <= vCount)
 			{
 				x++
-				$('#context').append("<p>" + jd.book[c].chapter[x].verse_nr + ": " + jd.book[c].chapter[x].verse +"</p>");
+				$('#context').append("<p><span class='sup'>" + jd.book[c].chapter[x].verse_nr + "</span> " + jd.book[c].chapter[x].verse +"</p>");
 			};  
 			
-    	
+  	});
+  	
     	
 		
  
 	 });
 								
-	// Control the Holy Flip button
-	$('#holyFlip').click(function() {
-		$("body").fadeOut(1000,reloadPage)	
-	});
+
+	
+		
 
 
 
