b029cf09c54b56b9cf7931a600e9bbc8e4cf041a
Marcus Gibbs <malchus@gmail.com>
2015-11-03 14:54:32 -0500

    Put comment js in another file until I’m ready for it.

--- j/holyflip.js
+++ j/holyflip.js
@@ -14,7 +14,7 @@ function getCookie () {			// Check for previous Session Cookie//
 	while (x < crumb) {
 		
 		if (breakCookie[x].indexOf("HFname") > 0){
-		$('<div>' + breakCookie[x] + '</div>').appendTo('body');
+		$('<div>' + breakCookie[x] + '</div>').appendTo('#account');
 		}
 		x++;
 	}
@@ -22,49 +22,6 @@ function getCookie () {			// Check for previous Session Cookie//
 }
 
 
-
-// Get Comments //
-function getComments () {
-$.ajax({
-    url: '/a/bible/serve/api-comments?nid=105509',
-    headers: {
-        'Authorization':'Basic dGVzdDpKYW1tZXIxMg==',
-        'Content-Type':'application/json'
-    },
-    success: function(data){
-      
-      $.each(data, function( a ) {
-  			$('<p>'+ (data[a].subject) + '</p>').appendTo('.comments')
-		});
-    }
-  });
-  
-}
-
-//End Get Comments
-
-
-
-//Post Comments
-
- function postComments () {
-	var theComment = {"nid":"105509","subject":"Comment Subject Text", "comment_body":{ "und":[{ "value":"Comment body text" }] }}
-
-
- 	$.ajax({
-  		type: "POST",
-    	url: '/a/bible/serve/comment.json',
-  		headers: {
-  		'Content-Type':'application/json',
-  		'Authorization':'Basic dGVzdDpKYW1tZXIxMg=='
-  		},
-  		data: JSON.stringify(theComment),
-  	
-		});
-
-  }
-
-// End Post Comments
 
 
 
